2b9094099684c74b5cd8abfbc3e5f998729ca1e5
Vishal Petkar <vishpetkar18@gmail.com>
2022-07-10 20:34:41 +0530

    Updated main.py file
    Added left and right movement functionality

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import pygame
 # initialize pygame
 pygame.init()
 
-# Create the screen
+# Create the screen (width, height)
 screen = pygame.display.set_mode((800, 600))
 
 # Title and Icon
@@ -11,12 +11,37 @@ pygame.display.set_caption("Space Invaders")
 icon = pygame.image.load("game_icon.png")
 pygame.display.set_icon(icon)
 
+# Player image
+playerImg = pygame.image.load("spaceship_icon.png")
+playerX = 370
+playerY = 480
+playerX_change = 0
+
+
+def player(x, y):
+    screen.blit(playerImg, (x, y))
+
+
 # Game loop
 running = True
 while running:
+    # RGB - red, green, blue
+    screen.fill((75, 150, 100))
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-    # RGB - red, green, blue
-    screen.fill((75, 150, 100))
+
+        # If keystroke is pressed, check if its right or left
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_a:  # can be if event.key == pygame.K_LEFT:
+                playerX_change = -0.5
+            if event.key == pygame.K_d:  # can be if event.key == pygame.K_RIGHT:
+                playerX_change = 0.5
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_a or event.key == pygame.K_d:
+                playerX_change = 0
+
+    playerX += playerX_change
+    player(playerX, playerY)
     pygame.display.update()
