3fa3a0b6b18e82eb488300279f5866e01d63f2cb
Vishal Petkar <vishpetkar18@gmail.com>
2022-07-10 17:49:55 +0530

    Changed the title and icon
    Code to change the title and add an icon to the game window.  Also screen.fill to color the screen with RGB values

--- main.py
+++ main.py
@@ -4,7 +4,12 @@ import pygame
 pygame.init()
 
 # Create the screen
-screen = pygame.display.set_mode((800,600))
+screen = pygame.display.set_mode((800, 600))
+
+# Title and Icon
+pygame.display.set_caption("Space Invaders")
+icon = pygame.image.load("game_icon.png")
+pygame.display.set_icon(icon)
 
 # Game loop
 running = True
@@ -12,3 +17,6 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+    # RGB - red, green, blue
+    screen.fill((75, 150, 100))
+    pygame.display.update()
